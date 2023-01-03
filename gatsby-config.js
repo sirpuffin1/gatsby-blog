1205b2c86cce0437ee191319ca8c3302e8cf0205
sirpuffin1 <angelcruz981@yahoo.com>
2023-01-02 16:08:09 -0800

    chore(meta): update site description

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@
 module.exports = {
   siteMetadata: {
     title: `Angel's Little Blog`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Possible the greatest blog ever made.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
